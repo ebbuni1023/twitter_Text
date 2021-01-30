6ec612ce8282e6a6c33630c7a8934ae86b55bf2d
Jiyoung Yoon <ebbuni1023@users.noreply.github.com>
2021-01-30 15:37:03 -0600

    updated

--- twitter_app/lib/main.dart
+++ twitter_app/lib/main.dart
@@ -1,61 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:twitter_app/login.dart';
+import 'login_page.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(new MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Registration'),
-        ),
-        body: Center(
-            child: Column(children: <Widget>[
-          Container(
-            margin: EdgeInsets.all(20),
-            child: FlatButton(
-              child: Text('Welcome to Twitter_Text'),
-              onPressed: () {},
-            ),
-          ),
-          TextField(
-            decoration: InputDecoration(
-              filled: true,
-              labelText: 'Email',
-            ),
-          ),
-          SizedBox(height: 12.0),
-          TextField(
-            decoration: InputDecoration(
-              filled: true,
-              labelText: 'User Name',
-            ),
-            obscureText: true,
-          ),
-          SizedBox(height: 12.0),
-          TextField(
-            decoration: InputDecoration(
-              filled: true,
-              labelText: 'Password',
-            ),
-            obscureText: true,
-          ),
-          Container(
-            margin: EdgeInsets.only(bottom: 20),
-            child: FlatButton(
-              child: Text('Sign Up'),
-              color: Colors.blue,
-              textColor: Colors.white,
-              onPressed: () {},
-            ),
-          ),
-        ])),
-        //home: SplashScreen(),
+    return new MaterialApp(
+      title: 'login demo',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: new LoginPage(),
     );
   }
 }
